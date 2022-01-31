cc537ce6a183c0748fd400e738fc8c26cfecd3bf
Roman <65005246+BukovskyiRoman@users.noreply.github.com>
2022-01-31 09:57:31 +0200

    Update and rename index.php to index.html

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$bot_data = json_decode(file_get_contents('php://input'), true);
-file_put_contents('file.txt', 'data: ' . print_r($bot_data, 1) . "\n", FILE_APPEND);
-
-https://api.telegram.org/bot5282211499:AAFfiqv28Xg7s_xY8_jZiJ2-1yNUKUKHBMo/setwebhook?url=https://github.com/BukovskyiRoman/BukovskyiRoman.github.io/index.php
